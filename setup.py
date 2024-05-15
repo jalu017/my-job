409a3a85bb66c2c2548f6c21eeb97e9d960935c3
Paulsen_gak_valid <68009524+jalu017@users.noreply.github.com>
2024-05-15 08:39:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
-from setuptools import setup, find_packages
- 
-classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
-]
- 
-setup(
-  name='myjobs',
-  version='0.0.1',
-  description='A very basic calculator',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
-  author='jalu017',
-  author_email='bebeknganjian@gmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords='pinjamduluseratus', 
-  packages=find_packages(),
-  install_requires=[''] 
-)
+initiad tx mstaking create-validator \
+--amount 1000000uinit \
+--pubkey $(initiad tendermint show-validator) \
+--moniker "YOUR_MONIKER_NAME" \
+--identity "YOUR_KEYBASE_ID" \
+--details "YOUR_DETAILS" \
+--website "YOUR_WEBSITE_URL" \
+--chain-id initiation-1 \
+--commission-rate 0.05 \
+--commission-max-rate 0.20 \
+--commission-max-change-rate 0.05 \
+--from wallet \
+--gas-adjustment 1.4 \
+--gas auto \
+--gas-prices 0.15uinit \
+-y
